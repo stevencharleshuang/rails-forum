32f2f9a70d78952e82a8ec78cf4f606822f60216
stevencharleshuang <steven.charles.huang@gmail.com>
2018-07-26 00:07:34 -0400

    interacting with params

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,9 @@
 class WelcomeController < ApplicationController
   def index
+  	if(params.has_key?(:id) && params.has_key?(:random))
+  		@id = params['id']
+  		@random = params[:random]
+  	end
   end
 
   def sample
